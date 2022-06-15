f647d594e2c9b88015a91f267591d55f2c02a81b
Akiyuki <akiyuki090@gmail.com>
2022-06-15 17:06:25 -0300

    Finalizando projeto

---
+++ script.js
@@ -0,0 +1,32 @@
+function insert(valor){
+
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + valor;
+
+}
+
+function clean(){
+
+    document.getElementById('resultado').innerHTML = '';
+
+}
+
+function apagar(){
+
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+
+}
+
+function calculo(){
+
+    var resultado = document.getElementById('resultado').innerHTML;
+
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else{
+        document.getElementById('resultado').innerHTML = 'Inserir valores';
+    }
+
+}
